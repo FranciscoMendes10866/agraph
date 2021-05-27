9afc41205838434049cfddd945fe33aaddb4ab11
FranciscoMendes10866 <avizinhadochico@gmail.com>
2021-05-27 22:56:46 +0100

    Client Boilerplate
    Installed all dependencies
    Including:
    Google fonts and Bootswatch

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'reactstrap'
+
+const App = () => {
+  return (
+    <>
+      <h2>Hello World 🍔</h2>
+      <Button color="primary">primary</Button>
+    </>
+  );
+}
+
+export default App;

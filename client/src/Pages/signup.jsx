e1f9c49c5c5f28a2f47d77001c5fbcea2162d506
FranciscoMendes10866 <avizinhadochico@gmail.com>
2021-05-28 12:11:13 +0100

    Auth Forms
    Added sign up and sign in forms

---
+++ client/src/Pages/signup.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import { Card, CardBody, CardTitle, CardSubtitle, Button, Form, FormGroup, Label, Input, Container, Row, Col } from 'reactstrap';
+
+const SignUp = () => {
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+    const handleOnChange = e => {
+        e.preventDefault()
+        const { name, value } = e.target
+        setForm({ ...form, [name]: value })
+    }
+    const handleOnRegister = e => {
+        e.preventDefault()
+        console.log(form)
+    }
+    return (
+        <Container>
+            <Row className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+                <Col xs="12" sm="10" md="5" lg="5">
+                    <Card>
+                        <CardBody>
+                            <CardTitle tag="h3" className="text-center">Welcome.</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted text-center mt-3">Create a new account</CardSubtitle>
+                            <Form className="mt-4" onSubmit={handleOnRegister}>
+                                <FormGroup>
+                                    <Label for="exampleEmail">Username</Label>
+                                    <Input onChange={handleOnChange} value={form.username} type="text" name="username" placeholder="Type your username" />
+                                </FormGroup>
+                                <FormGroup className="mt-4">
+                                    <Label for="examplePassword">Email</Label>
+                                    <Input onChange={handleOnChange} value={form.email} type="email" name="email" placeholder="Type your email" />
+                                </FormGroup>
+                                <FormGroup className="mt-4">
+                                    <Label for="examplePassword">Password</Label>
+                                    <Input onChange={handleOnChange} value={form.password} type="password" name="password" placeholder="Type your password" />
+                                </FormGroup>
+                                <Button color="primary" className="mt-5" type="submit">Register</Button>
+                            </Form>
+                        </CardBody>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default SignUp;
